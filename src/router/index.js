cc045a67fd0ac931096785a08d6f5730e0a1ac9b
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-10 10:56:05 -0300

    feat: create NotFound page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import store from '@/store'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -23,7 +25,22 @@ const routes = [
         props: true,
         component: () => import('../views/ExperienceDetails.vue')
       }
-    ]
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(d => d.slug === to.params.slug)
+
+      if (exists) {
+        return next()
+      }
+
+      next({ name: 'NotFound' })
+    }
+  },
+  {
+    path: '/404',
+    alias: '*',
+    name: 'NotFound',
+    component: () => import('../views/NotFound.vue')
   }
 ]
 
