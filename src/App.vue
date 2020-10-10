26e203f255fc13427905958cfb836a631af8fc03
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-09 21:42:08 -0300

    feat: add links and images

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,20 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Brazil from '../views/Brazil'
+import Jamaica from '../views/Jamaica'
+import Hawaii from '../views/Hawaii'
+import Panama from '../views/Panama'
 
 Vue.use(VueRouter)
 
@@ -11,13 +15,33 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/brazil',
+    name: 'Brazil',
+    component: Brazil
+  },
+  {
+    path: '/jamaica',
+    name: 'Jamaica',
+    component: Jamaica
+  },
+  {
+    path: '/hawaii',
+    name: 'Hawaii',
+    component: Hawaii
+  },
+  {
+    path: '/panama',
+    name: 'Panama',
+    component: Panama
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
