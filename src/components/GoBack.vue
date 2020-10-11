a1581ca7a2b11ba64a69ad8b681a58fdee28973f
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-11 20:29:40 -0300

    style: add goback style

--- src/components/GoBack.vue
+++ src/components/GoBack.vue
@@ -13,3 +13,18 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+span {
+  position: absolute;
+  top: calc(80px + 20px);
+  left: 50px;
+}
+
+button {
+  border: none;
+  background-color: transparent;
+  font-size: 1rem;
+  color: var(--green);
+}
+</style>
