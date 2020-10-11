3fb722b1dd7ea41921ef33d99da6bbf48d86a3bf
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-11 20:14:26 -0300

    style: refactor login styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div id="view-login">
     <h1>Login</h1>
     <div>
       <label for="username">Username</label>
-      <input v-model="username" type="text" name="usernaem">
+      <input v-model="username" type="text" name="usernaem" autofocus>
       <label for="password">Password</label>
       <input v-model="password" type="text" name="password">
-      <button @click="login">Login</button>
+      <button @click="login">Enter</button>
     </div>
   </div>
 </template>
@@ -30,3 +30,66 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+#view-login {
+  height: calc(100vh - 80px);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 100px;
+  align-items: center;
+}
+
+#view-login div {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+#view-login div input {
+  width: 300px;
+  height: 36px;
+  margin: 0px 0px 10px;
+  border: 2px solid lightgray;
+  border-radius: 2.5px;
+  padding: 0 10px;
+  transition: border .2s ease-out;
+  font-size: 14px;
+}
+
+#view-login div input:focus {
+  border-color: var(--green);
+}
+
+#view-login div label {
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+#view-login div button {
+  margin-top: 10px;
+  border: none;
+  background-color: var(--green);
+  font-size: 1rem;
+  display: grid;
+  place-items: center;
+  height: 36px;
+  width: 100%;
+  padding: 0 30px;
+  border-radius: 2.5px;
+  color: var(--black);
+  text-transform: uppercase;
+  font-weight: bold;
+  cursor: pointer;
+  opacity: 1;
+  transition: opacity 100ms ease-out;
+}
+
+#view-login div button:hover {
+  opacity: 0.85;
+}
+
+</style>
