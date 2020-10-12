9f740dc210d82c9675c377979e2de34b17f792c6
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-11 21:06:56 -0300

    style: refactor not found style

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,7 +3,26 @@
     <h1>Not Found</h1>
     <p>
       Oops we coudn't find that page. Try going
-      <router-link :to="{ name: 'Home' }">home</router-link>
+      <router-link :to="{ name: 'Home' }">home!</router-link>
     </p>
   </div>
 </template>
+
+<style scoped>
+div {
+  display: grid;
+  place-items: center;
+  place-content: center;
+  height: calc(100vh - 80px);
+}
+
+h1 {
+  margin-bottom: 50px;
+  color: var(--green);
+}
+
+p a {
+  color: var(--green);
+  text-decoration: none;
+}
+</style>
